264fb57cc22f71b47a41eda0d93a00ff78af8aa6
Rossstarific <37845230+Rossstarific@users.noreply.github.com>
2019-12-10 22:34:41 -0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -52,16 +52,16 @@
 .equ		dir_up = 0b00001000    ; PC3 for up
 
 ; Giving the registers used later names
-.def		y_pos = r19			   ; setting Y position on screen
-.def		x_pos = r18			   ; setting X position on screen
+.def		y_pos = r19		   ; setting Y position on screen
+.def		x_pos = r18		   ; setting X position on screen
 .def		sprite = r17		   ; storing the sprite written to screen
 .def		workhorse = r29		   ; generic temp register for multiple uses
 ; These next two are for keeping track of the overall (global) position of the player, 
 ; so that the x_pos and y_pos registers may be manipulated if necessary
-.def		position_count_x = r14 ; for saving the global x position of the player
-.def		position_count_y = r13 ; for saving the global y position of the player
-.def		state = r28			   ; for telling the program what button was pressed
-.def		score_counter = r23    ; for keeping track of how many spaces the player has moved, i.e. the "score"
+.def		position_count_x = r14     ; for saving the global x position of the player
+.def		position_count_y = r13     ; for saving the global y position of the player
+.def		state = r28	           ; for telling the program what button was pressed
+.def		score_counter = r23        ; for keeping track of how many spaces the player has moved, i.e. the "score"
 
 .cseg
 .org		0x0000
@@ -82,23 +82,23 @@ setup:
 	rcall		GFX_refresh_screen
 
 	; Setting up the PORT C pins as inputs, then setting PC0-3 as capable of causing interrupts on the falling edge
-	ldi			workhorse, 0b00000000
-	sts			PORTC_DIR, workhorse
-	ldi			workhorse, 0b00001011
-	sts			PORTC_PIN0CTRL, workhorse
-	sts			PORTC_PIN1CTRL, workhorse
-	sts			PORTC_PIN2CTRL, workhorse
-	sts			PORTC_PIN3CTRL, workhorse
+	ldi		workhorse, 0b00000000
+	sts		PORTC_DIR, workhorse
+	ldi		workhorse, 0b00001011
+	sts		PORTC_PIN0CTRL, workhorse
+	sts		PORTC_PIN1CTRL, workhorse
+	sts		PORTC_PIN2CTRL, workhorse
+	sts		PORTC_PIN3CTRL, workhorse
 
 	; Setting various other game related things
-	ldi			score_counter, 0			; initializing score counter
-	ldi			state, dir_right			; initializing state with the default direction: right
-	ldi			workhorse, 0				
-	mov			position_count_x, workhorse ; initializing global x position
-	mov			position_count_y, workhorse ; initializing global y position
-	mov			x_pos, position_count_x		; initializing x position for the screen
-	mov			y_pos, position_count_y		; initializing y position for the screen
-	rcall		GFX_set_array_pos			; function in GFX library that actually sets the position on screen using x_pos and y_pos
+	ldi		score_counter, 0		; initializing score counter
+	ldi		state, dir_right		; initializing state with the default direction: right
+	ldi		workhorse, 0				
+	mov		position_count_x, workhorse     ; initializing global x position
+	mov		position_count_y, workhorse     ; initializing global y position
+	mov		x_pos, position_count_x		; initializing x position for the screen
+	mov		y_pos, position_count_y		; initializing y position for the screen
+	rcall		GFX_set_array_pos		; function in GFX library that actually sets the position on screen using x_pos and y_pos
 
 	; Enables global interrupts
 	sei
@@ -118,30 +118,30 @@ main_loop:
 	rcall		delay_10ms
 	; 2.
 	bound_check:
-		mov			x_pos, position_count_x
-		mov			y_pos, position_count_y
-		cpi			x_pos, bound_left
+		mov		x_pos, position_count_x
+		mov		y_pos, position_count_y
+		cpi		x_pos, bound_left
 		breq		game_over
-		cpi			x_pos, bound_right
+		cpi		x_pos, bound_right
 		breq		game_over
-		cpi			y_pos, bound_upper
+		cpi		y_pos, bound_upper
 		breq		game_over
-		cpi			y_pos, bound_lower
+		cpi		y_pos, bound_lower
 		breq		game_over
 		rcall		GFX_set_array_pos
 	; 3.
 	score_check:
-		inc			score_counter
-		cpi			score_counter, 200
+		inc		score_counter
+		cpi		score_counter, 200
 		breq		game_win
 	; 4.
-	cpi			state, dir_right
+	cpi		state, dir_right
 	breq		loop_right
-	cpi			state, dir_left
+	cpi		state, dir_left
 	breq		loop_left
-	cpi			state, dir_down
+	cpi		state, dir_down
 	breq		loop_down
-	cpi			state, dir_up
+	cpi		state, dir_up
 	breq		loop_up
 	loop_right:
 		rjmp		draw_right
@@ -155,11 +155,11 @@ main_loop:
 	; This is to allow for the two subroutines called within them to be interrupted more smoothly.
 	game_over:
 		rcall		GFX_clear_array
-		ldi			state, 0b00000000
+		ldi		state, 0b00000000
 		rjmp		game_over_reset
 	game_win:
 		rcall		GFX_clear_array
-		ldi			state, 0b00000000
+		ldi		state, 0b00000000
 		rjmp		game_win_reset
 ;---------------------------------------------------------------------------------
 
@@ -167,29 +167,27 @@ main_loop:
 ; For drawing the two right-iterated sprites. It draws the ship sprite first, decrements x_pos, then draws the fire sprite.
 ; After this, it increments the global x position (via position_count_x)
 draw_right:
-	ldi			sprite, ship_R
-	st			X, sprite
+	ldi		sprite, ship_R
+	st		X, sprite
 	rcall		next_char_R
-    ldi			sprite, fire_R
-	st			X, sprite
-	rcall		next_char_R
+        ldi		sprite, fire_R
+	st		X, sprite
 	; the two following instructions are used to update the screen with the sprites, then clear the array so that there are no leftover sprites
 	; that fill up the screen.
 	rcall		GFX_refresh_screen
 	rcall		GFX_clear_array
-	inc			position_count_x
+	inc		position_count_x
 	; jump back up to main loop to repeat the bound check, score check, and direction check processes
 	rjmp		main_loop
 
 ; Draws left-oriented sprites. Only difference is that it increments x_pos after drawing the first sprite in order to draw the second sprite.
 ; It also decrements the the global x position (via position_count_x)
 draw_left:
-	ldi			sprite, ship_L
-	st			X, sprite
+	ldi		sprite, ship_L
+	st		X, sprite
 	rcall		next_char_L
-    ldi			sprite, fire_L
-	st			X, sprite
-	rcall		next_char_L
+    	ldi		sprite, fire_L
+	st		X, sprite
 	rcall		GFX_refresh_screen
 	rcall		GFX_clear_array
 	dec			position_count_x
@@ -198,52 +196,50 @@ draw_left:
 ; Draws downward_oriented sprites. It draws the first sprite, increments the y_pos, then draws the second sprite.
 ; It then increments the global y position (via position_count_y) 
 draw_down:
-	ldi			sprite, ship_D
-	st			X, sprite
+	ldi		sprite, ship_D
+	st		X, sprite
 	rcall		next_char_D
-	ldi			sprite, fire_D
-	st			X, sprite
-	rcall		next_char_D
+	ldi		sprite, fire_D
+	st		X, sprite
 	rcall		GFX_refresh_screen
 	rcall		GFX_clear_array
-	inc			position_count_y
+	inc		position_count_y
 	rjmp		main_loop
 
 ; Draws upward_oriented sprites. Draws first sprite, decrements y_pos, then draws second sprite.
 ; Moves upward by decrementing the global y position (via position_count_y)
 draw_up:
-	ldi			sprite, ship_U
-	st			X, sprite
+	ldi		sprite, ship_U
+	st		X, sprite
 	rcall		next_char_U
-	ldi			sprite, fire_U
-	st			X, sprite
-	rcall		next_char_U
+	ldi		sprite, fire_U
+	st		X, sprite
 	rcall		GFX_refresh_screen
 	rcall		GFX_clear_array
-	dec			position_count_y
+	dec		position_count_y
 	rjmp		main_loop
 	
 ; Subroutine for decrementing x_pos to draw the next right-oriented sprite
 next_char_R:
-	dec			x_pos
+	dec		x_pos
 	rcall		GFX_set_array_pos
 	ret			
 
 ; Subroutine for incrementing x_pos to draw the next left-oriented sprite
 next_char_L:
-	inc			x_pos
+	inc		x_pos
 	rcall		GFX_set_array_pos
 	ret
 
 ; Subroutine for incrementing y_pos to draw the next downward-oriented sprite
 next_char_D:
-	dec			y_pos
+	dec		y_pos
 	rcall		GFX_set_array_pos
 	ret
 
 ; Subroutine for decrementing y_pos to draw the next upward-oriented sprite
 next_char_U:
-	inc			y_pos
+	inc		y_pos
 	rcall		GFX_set_array_pos
 	ret
 
@@ -251,85 +247,85 @@ next_char_U:
 ; It loops until any button is pressed, at which point it jumps to the main loop and starts the movement in the default direction 
 game_over_reset:
 	; All for writing "GAME  OVER" at the center of the screen
-	ldi			x_pos, 3
-	ldi			y_pos, 4
+	ldi		x_pos, 3
+	ldi		y_pos, 4
 	rcall		GFX_set_array_pos
-	ldi			sprite, char_G
-	st			X+, sprite
-	ldi			sprite, char_A
-	st			X+, sprite
-	ldi			sprite, char_M
-	st			X+, sprite
-	ldi			sprite, char_E
-	st			X+, sprite
-	ldi			sprite, blank
-	st			X+, sprite
-	ldi			sprite, blank
-	st			X+, sprite
-	ldi			sprite, char_O
-	st			X+, sprite
-	ldi			sprite, char_V
-	st			X+, sprite
-	ldi			sprite, char_E
-	st			X+, sprite
-	ldi			sprite, char_R
-	st			X+, sprite
+	ldi		sprite, char_G
+	st		X+, sprite
+	ldi		sprite, char_A
+	st		X+, sprite
+	ldi		sprite, char_M
+	st		X+, sprite
+	ldi		sprite, char_E
+	st		X+, sprite
+	ldi		sprite, blank
+	st		X+, sprite
+	ldi		sprite, blank
+	st		X+, sprite
+	ldi		sprite, char_O
+	st		X+, sprite
+	ldi		sprite, char_V
+	st		X+, sprite
+	ldi		sprite, char_E
+	st		X+, sprite
+	ldi		sprite, char_R
+	st		X+, sprite
 	rcall		GFX_refresh_screen
-	cpi			state, 0b00000000
+	cpi		state, 0b00000000
 	brne		game_over_end
 	rjmp		game_over_reset
 	; Triggered when a button is pressed: clears game over screen and resets all position data.
 	; It also sets the state register to the default direction.
 	game_over_end:
-		ldi			score_counter, 0
-		ldi			state, dir_right
+		ldi		score_counter, 0
+		ldi		state, dir_right
 		rcall		GFX_clear_array
-		ldi			workhorse, 0
-		mov			position_count_x, workhorse
-		mov			position_count_y, workhorse
+		ldi		workhorse, 0
+		mov		position_count_x, workhorse
+		mov		position_count_y, workhorse
 		rjmp		main_loop
 
 ; Subroutine that flashes "YOU WIN!" on the screen until a button is pressed, at which point it resets the game the exact same way
 ; that "game_over_reset" does. It is triggered when the player has successfully moved 200 spaces without touching the boundaries.
 game_win_reset:
-	ldi			x_pos, 4
-	ldi			y_pos, 4
+	ldi		x_pos, 4
+	ldi		y_pos, 4
 	rcall		GFX_set_array_pos
-	ldi			sprite, char_Y
-	st			X+, sprite
-	ldi			sprite, char_O
-	st			X+, sprite
-	ldi			sprite, char_U
-	st			X+, sprite
-	ldi			sprite, blank
-	st			X+, sprite
-	ldi			sprite, char_W
-	st			X+, sprite
-	ldi			sprite, char_I
-	st			X+, sprite
-	ldi			sprite, char_N
-	st			X+, sprite
-	ldi			sprite, char_exc_point
-	st			X+, sprite
+	ldi		sprite, char_Y
+	st		X+, sprite
+	ldi		sprite, char_O
+	st		X+, sprite
+	ldi		sprite, char_U
+	st		X+, sprite
+	ldi		sprite, blank
+	st		X+, sprite
+	ldi		sprite, char_W
+	st		X+, sprite
+	ldi		sprite, char_I
+	st		X+, sprite
+	ldi		sprite, char_N
+	st		X+, sprite
+	ldi		sprite, char_exc_point
+	st		X+, sprite
 	rcall		GFX_refresh_screen
 	rcall		delay_100ms
 	rcall		GFX_clear_array
 	rcall		GFX_refresh_screen
 	rcall		delay_100ms
-	cpi			state, 0b00000000
+	cpi		state, 0b00000000
 	brne		game_win_end
 	rjmp		game_win_reset
 	; Does the exact same thing as "game_over_end"
 	game_win_end:
-		ldi			score_counter, 0
-		ldi			state, 0b00000001
+		ldi		score_counter, 0
+		ldi		state, 0b00000001
 		rcall		GFX_clear_array
-		ldi			x_pos, 0
-		ldi			y_pos, 0
+		ldi		x_pos, 0
+		ldi		y_pos, 0
 		rcall		GFX_set_array_pos
-		ldi			workhorse, 0
-		mov			position_count_x, workhorse
-		mov			position_count_y, workhorse
+		ldi		workhorse, 0
+		mov		position_count_x, workhorse
+		mov		position_count_y, workhorse
 		rjmp		main_loop	
 ;---------------------------------------------------------------------------------
 
@@ -338,8 +334,8 @@ game_win_reset:
 ; It sets the "state" register to the value that represents whatever PORT C pin had a button press (i.e. 0b00000001 for PC0, etc).
 ; It then resets all interrupt flags so that further interrupts can be triggered. 
 change_dir:
-	lds			state, PORTC_INTFLAGS
-	ldi			workhorse, 0b00001111
-	sts			PORTC_INTFLAGS, workhorse
+	lds		state, PORTC_INTFLAGS
+	ldi		workhorse, 0b00001111
+	sts		PORTC_INTFLAGS, workhorse
 	reti
 ;---------------------------------------------------------------------------------
